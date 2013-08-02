357b48eaf6a6b98b1410ceb3e59768bd00ce4109
a.cianfarani <a.cianfarani@c2is.fr>
2013-08-02 11:25:56 +0200

    Added unit tests

---
+++ tests/units/Walker.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the C2iS <http://wwww.c2is.fr/> checkLampConf project.
+ * André Cianfarani <a.cianfarani@c2is.fr>
+ */
+
+
+namespace tests\units;
+
+require_once 'vendor/bin/atoum';
+
+include realpath(__DIR__) . '/../../Walker/Walker.php';
+
+use \mageekguy\atoum;
+use \Walker;
+
+class Walker extends atoum\test
+{
+    public function test__construct()
+    {
+        $this->string("Hello World!")->isEqualTo('Hello World!');
+    }
+}
